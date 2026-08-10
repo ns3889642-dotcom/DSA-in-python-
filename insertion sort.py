61a5d78dfed59c01bc181db90489d51e5aee122f
ns3889642-dotcom <ns3889642@gmail.com>
2026-08-10 20:37:35 +0530

    Add files via upload

---
+++ insertion sort.py
@@ -0,0 +1,16 @@
+nums = [3, 4, 1, 2, 7, 5, 9, 8]
+
+def insertion_sort(arr):
+    n = len(arr)
+    for i in range(1, n):
+        key = arr[i]
+        j = i - 1
+
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+
+            arr[j + 1] = key
+
+insertion_sort(nums)
+print("Sorted Array: ", nums)
